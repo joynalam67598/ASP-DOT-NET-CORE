5169f2f8a46d52e310dd2801372318cd68221c06
joynal <joynalam67598@gmail.com>
2021-07-04 21:28:26 +0600

    convert console app to web app

---
+++ Console App To Web App/FirstConsoleApp/FirstConsoleApp/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FirstConsoleApp
+{
+    class StartUp
+    {
+        public void ConfigurationService(IServiceCollection servive)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello form Console To Web APP");
+                });
+            });
+        }
+    }
+}
