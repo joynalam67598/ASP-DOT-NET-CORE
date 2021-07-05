b49992694d100724a31a71aaa9c1213de7ba8238
joynal <joynalam67598@gmail.com>
2021-07-05 21:48:48 +0600

    model and repository

---
+++ BookStoreWebApp/BookStoreWebApp/Repository/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using BookStoreWebApp.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreWebApp.Repository
+{
+    public class BookRepository
+    {
+        // we can write all kind of logic here , works like provider
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        public BookModel GetBookById( int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+        public List<BookModel> SecarchBook(string title, string author)
+        {
+            return DataSource().Where(x => x.Title == title && x.Author == author).ToList();
+        }
+        public List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id =  1 , Title = "Dot" , Author = "I"},
+                new BookModel(){Id =  2 , Title = "Net" , Author = "He"},
+                new BookModel(){Id =  3 , Title = "Core" , Author = "They"},
+
+            };
+        }
+
+    }
+}
---
+++ BookStoreWebApp/BookStoreWebApp/controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using BookStoreWebApp.model;
+using BookStoreWebApp.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreWebApp.Controllers
+{
+    public class BookController : Controller
+    {
+        // type of the repository.
+        public readonly BookRepository _bookRepository = null;
+
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+
+        public BookModel GetBook(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModel> SearchBooks(string bookName, string authorName)
+        {
+            return _bookRepository.SecarchBook(bookName, authorName);
+        }
+    }
+}
